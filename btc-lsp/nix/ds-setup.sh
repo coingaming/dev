8d23cd7bd4dcaeb5926ece0176772526beaeaeef
tkachuk-labs <tkachuk.labs@gmail.com>
2022-01-20 15:10:00 +0200

    better generate-tls-cert

--- btc-lsp/nix/ds-setup.sh
+++ btc-lsp/nix/ds-setup.sh
@@ -15,31 +15,8 @@ docker swarm init || true
 docker network create -d overlay --attachable global || true
 
 echo "==> keys generation"
-echo "subjectAltName=IP:127.0.0.1,DNS:localhost" \
-     > "$BUILD_DIR/subjectAltName"
 sh "$THIS_DIR/shell-docker.sh" --mini \
-   "--run 'cd ./build && \
-   echo DEPRECATED_ECDSA && \
-   openssl ecparam -name secp256k1 -genkey \
-     -noout -outform PEM -out esdsa.prv && \
-   openssl ec -inform PEM -in esdsa.prv \
-     -pubout -outform PEM -out esdsa.pub && \
-   echo LSP_TLS && \
-   openssl genrsa -out btc_lsp_tls_key.pem 2048 && \
-   openssl req -new -key btc_lsp_tls_key.pem \
-     -out csr.csr -subj /CN=btc-lsp/O=btc-lsp && \
-   openssl x509 -req -in csr.csr \
-     -extfile subjectAltName \
-     -signkey btc_lsp_tls_key.pem -out btc_lsp_tls_cert.pem && \
-   rm csr.csr && \
-   echo LND_TLS && \
-   openssl ecparam -genkey -name prime256v1 -out lnd_tls.key && \
-   openssl req -new -sha256 -key lnd_tls.key \
-     -out csr.csr -subj /CN=lnd/O=lnd && \
-   openssl req -x509 -sha256 -days 36500 -key lnd_tls.key \
-     -in csr.csr -out lnd_tls.cert && \
-   rm csr.csr'
-   "
+   "--run './nix/generate-tls-cert.sh'"
 
 echo "==> docker image build"
 sh "$THIS_DIR/release-docker.sh"
--- btc-lsp/nix/generate-tls-cert.sh
+++ btc-lsp/nix/generate-tls-cert.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 THIS_DIR="$(dirname "$(realpath "$0")")"
-BUILD_DIR="$THIS_DIR/../build"
+ROOT_DIR="$THIS_DIR/.."
+BUILD_DIR="$ROOT_DIR/build"
+LND_DIR="$ROOT_DIR/.lnd"
 
 mkdir -p "$BUILD_DIR"
 
@@ -12,14 +14,27 @@ echo "subjectAltName=IP:127.0.0.1,DNS:localhost" \
   #
   # TODO : generate individually for every lnd instance
   #
-  cd ./.lnd
+  echo "==> Generating LND TLS cert"
+  cd "$LND_DIR"
   openssl ecparam -genkey -name prime256v1 -out tls.key
-  openssl req -new -sha256 -key tls.key -out csr.csr -subj '/CN=localhost/O=lnd'
+  openssl req -new -sha256 -key tls.key \
+    -out csr.csr -subj /CN=lnd/O=lnd
   openssl x509 -req -in csr.csr \
     -sha256 -days 36500 \
     -extfile "$BUILD_DIR/subjectAltName" \
     -signkey tls.key -out tls.cert
   rm csr.csr
+  cp "$LND_DIR/tls.key" "$BUILD_DIR/lnd_tls.key"
+  cp "$LND_DIR/tls.cert" "$BUILD_DIR/lnd_tls.cert"
+)
+
+(
+  echo "==> Generating deprecated ECDSA keypair"
+  cd "$BUILD_DIR"
+  openssl ecparam -name secp256k1 -genkey \
+    -noout -outform PEM -out esdsa.prv
+  openssl ec -inform PEM -in esdsa.prv \
+    -pubout -outform PEM -out esdsa.pub
 )
 
 (
@@ -34,4 +49,4 @@ echo "subjectAltName=IP:127.0.0.1,DNS:localhost" \
   rm csr.csr
 )
 
-echo "==> Generated TLS certs"
+echo "==> Generated certs"
