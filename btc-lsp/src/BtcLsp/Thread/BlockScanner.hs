dcd1087c6acb52efdea03f8135b9293d42c46f93
drownbes <drownbes@gmail.com>
2022-03-04 16:52:34 +0200

    wip

--- btc-lsp/src/BtcLsp/Thread/BlockScanner.hs
+++ btc-lsp/src/BtcLsp/Thread/BlockScanner.hs
@@ -12,7 +12,10 @@ import BtcLsp.Import
 import qualified BtcLsp.Storage.Model.Block as Block
 import qualified Network.Bitcoin as Btc
 import qualified Data.Set as S
+import qualified Data.Map as M
 import qualified Data.Vector as V
+import qualified BtcLsp.Rpc.ElectrsRpc as Rpc
+import qualified BtcLsp.Rpc.Helper as Rpc
 
 
 apply :: (Env m) => m ()
@@ -22,6 +25,15 @@ apply = do
   sleep $ MicroSecondsDelay 1000000
   apply
 
+
+askElectrs :: (Env m) => Set Btc.Address -> ExceptT Failure m (Map Btc.Address MSat)
+askElectrs addrs = do
+  ab <- mapM askConfBalance (S.toList addrs)
+  pure $ M.fromList ab
+  where
+    askConfBalance addr = do
+      cb <- Rpc.confirmed <$> withElectrsT Rpc.getBalance ($ Left $ OnChainAddress addr)
+      pure (addr, cb)
 
 getBlockAddresses :: Btc.BlockVerbose -> Set Btc.Address
 getBlockAddresses blk = do
@@ -36,34 +48,31 @@ heighToInt :: BlkHeight -> Integer
 heighToInt (BlkHeight c) = c
 
 scan :: (Env m)
-     => ExceptT Failure m (Set Btc.Address)
+     => ExceptT Failure m (Map Btc.Address MSat)
 scan = do
   mBlk <- lift Block.getLatest
   cHeight <- into @BlkHeight <$> withBtcT Btc.getBlockCount id
+  void $ Rpc.waitTillLastBlockProcessedT $ heighToInt cHeight
   case mBlk of
     Nothing -> scanOneBlock cHeight
     Just lBlk -> do
       let s = heighToInt $ blockHeight $ entityVal lBlk
-      step S.empty (1 + s) (heighToInt cHeight)
+      let e = heighToInt cHeight
+      step M.empty (1 + s) e
     where
       step acc cur end = do
-        if cur == end
-           then do
-             addrs <- scanOneBlock $ BlkHeight cur
-             pure (acc <> addrs)
+        if cur > end
+           then pure acc
            else do
-             r <- step acc (cur + 1) end
-             pure (acc <> r)
-
+             addrs <- scanOneBlock $ BlkHeight cur
+             step (acc <> addrs) (cur + 1) end
 
 scanOneBlock :: (Env m) =>
-  BlkHeight -> ExceptT Failure m (Set Btc.Address)
+  BlkHeight -> ExceptT Failure m (Map Btc.Address MSat)
 scanOneBlock height = do
   hash <- withBtcT Btc.getBlockHash ($ from height)
   blk <- withBtcT Btc.getBlockVerbose ($ hash)
-  traceShowM blk
   prevHash <- ExceptT $ pure $ maybeToRight (FailureInternal "gen block") (Btc.vPrevBlock blk)
+  r <- askElectrs $ getBlockAddresses blk
   lift . void $ Block.createUpdate height (from hash) (from prevHash)
-  let r = getBlockAddresses blk
-  traceShowM r
   pure r
--- btc-lsp/test/BlockScannerSpec.hs
+++ btc-lsp/test/BlockScannerSpec.hs
@@ -13,15 +13,40 @@ import qualified BtcLsp.Thread.Main as Main
 import LndClient.LndTest (mine)
 import qualified Network.Bitcoin.Wallet as Btc
 import Test.Hspec
+import qualified BtcLsp.Rpc.ElectrsRpc as Rpc
+import BtcLsp.Rpc.Helper (waitTillLastBlockProcessedT)
+import qualified LndClient.Data.NewAddress as Lnd
+import qualified LndClient.RPC.Katip as Lnd
 
 spec :: Spec
 spec = do
   itEnv "Block scanner" $ do
     withSpawnLink Main.apply . const $ do
       sleep $ MicroSecondsDelay 500000
-      _r <- runExceptT $
-        withBtcT Btc.sendToAddress (\h -> h "bcrt1qxfrzgen4mcru4p23nthqjezf2f50taz6ls4w8x" 1 Nothing Nothing)
-      mine 1 LndLsp
+      addrResponse <-
+        runExceptT $
+          withLndT
+            Lnd.newAddress
+            ( $
+                Lnd.NewAddressRequest
+                  { Lnd.addrType = Lnd.WITNESS_PUBKEY_HASH,
+                    Lnd.account = Nothing
+                  }
+            )
+
+      mine 10 LndLsp
+      case addrResponse of
+        Left _ -> error "Error getting address from lnd"
+        Right response -> do
+            let trAddr = coerce response
+            _r <- runExceptT $ do
+              withBtcT Btc.sendToAddress (\h -> h trAddr 1 Nothing Nothing)
+            mine 10 LndLsp
+            sleep $ MicroSecondsDelay $ 10 * 1000000
+            b <- runExceptT $ do
+              _ <- waitTillLastBlockProcessedT 100
+              withElectrsT Rpc.getBalance ($ Left $ OnChainAddress trAddr)
+            traceShowM b
       sleep $ MicroSecondsDelay $ 2 * 1000000
       liftIO $ shouldBe True True
 
