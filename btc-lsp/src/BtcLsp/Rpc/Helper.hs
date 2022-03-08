c27d9b8f950ff9a565330dfa8a6953200b53eb0a
MikhailPrushinskiy <mikhail.prushinskiy@heathmont.net>
2022-03-08 14:21:52 +0200

    Refactor

--- btc-lsp/src/BtcLsp/Rpc/Helper.hs
+++ btc-lsp/src/BtcLsp/Rpc/Helper.hs
@@ -2,16 +2,16 @@
 
 module BtcLsp.Rpc.Helper
   ( waitTillLastBlockProcessedT,
-    waitTillLastBlockProcessed
+    waitTillLastBlockProcessed,
   )
 where
 
 import BtcLsp.Import
 import BtcLsp.Rpc.ElectrsRpc as Rpc
+import qualified Data.ByteString.Lazy as BS
+import Data.Digest.Pure.SHA (bytestringDigest, sha256)
 import Network.Bitcoin (getBlockCount, getBlockHash)
 import qualified Text.Hex as TH
-import Data.Digest.Pure.SHA (bytestringDigest, sha256)
-import qualified Data.ByteString.Lazy as BS
 
 waitTillLastBlockProcessed :: Env m => Integer -> m (Either Failure ())
 waitTillLastBlockProcessed = runExceptT . waitTillLastBlockProcessedT
@@ -23,25 +23,15 @@ waitTillLastBlockProcessedT decr = do
   bHeight <- withBtcT getBlockCount id
   bHash <- withBtcT getBlockHash ($ bHeight)
   bHeader <- withElectrsT Rpc.blockHeader ($ BlkHeight (coerce bHeight))
-  let bh = TH.decodeHex (coerce bHeader)
---  let bh0 = case bh of
---              Just x -> x
---              Nothing -> error "1"
-  let bh2 = BS.toStrict . BS.reverse . bytestringDigest . sha256 . bytestringDigest . sha256 . BS.fromStrict <$> bh
-  let maybeBhx = TH.decodeHex (coerce $ Rpc.BlockHeader bHash)
---  let bhx0 = case maybeBhx of
---               Just y -> y
---               Nothing -> error "2"
-  if bh2 == maybeBhx then
-    return ()
-  else
-    waitTillLastBlockProcessedT (decr -1)
---  case bHeader of
---    Rpc.BlockHeader "" -> waitTillLastBlockProcessedT (decr -1)
---    _ -> return ()
-
--- TODO Implement blockHash comparison, now it looks like has different formats so doesn't match
---  if bHeader == Rpc.BlockHeader bHash then
---    return ()
---  else
---    waitTillLastBlockProcessedT (decr -1)
+  if (doubleSha256AndReverse <$> TH.decodeHex (coerce bHeader)) == TH.decodeHex (coerce $ Rpc.BlockHeader bHash)
+    then return ()
+    else waitTillLastBlockProcessedT (decr -1)
+  where
+    doubleSha256AndReverse =
+      BS.toStrict
+        . BS.reverse
+        . bytestringDigest
+        . sha256
+        . bytestringDigest
+        . sha256
+        . BS.fromStrict
