946e36ad27d2f77cd69cf5025f39d0d5f5c50453
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2022-04-01 12:45:07 +0200

    Delete docker swarm files

--- btc-lsp/nix/ds-psql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-docker exec -it \
-  `docker ps -f name=yolo_postgres --quiet` \
-  psql -U btc-lsp btc-lsp
--- btc-lsp/nix/ds-sh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-docker exec -it \
-  `docker ps -f name=$1 --quiet` \
-  sh
