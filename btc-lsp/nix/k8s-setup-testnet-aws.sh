7a718ac4306dcf08f5c426906351c26e0ffcd294
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2022-04-27 15:01:03 +0200

    Use proper camelcase for functions

--- btc-lsp/nix/k8s-setup-testnet-aws.sh
+++ btc-lsp/nix/k8s-setup-testnet-aws.sh
@@ -8,11 +8,11 @@ THIS_DIR="$(dirname "$(realpath "$0")")"
 
 BITCOIN_NETWORK="testnet"
 CLOUD_PROVIDER="aws"
-DB_USERNAME="lsp"
-TMP_DIR="/tmp"
-K8S_CLUSTER_NAME="lsp-$BITCOIN_NETWORK"
-DB_INSTANCE_NAME="$K8S_CLUSTER_NAME"
+DATABASE_USERNAME="lsp"
 IDEMPOTENCY_TOKEN=$(date +%F | md5 | cut -c1-5)
+
+KUBERNETES_CLUSTER_NAME="lsp-$BITCOIN_NETWORK"
+DATABASE_INSTANCE_NAME="$KUBERNETES_CLUSTER_NAME"
 
 isAwsConfigured () {
   for VARNAME in "aws_access_key_id" "aws_secret_access_key" "region"; do
@@ -23,26 +23,33 @@ isAwsConfigured () {
   done
 }
 
-createIAMOpenIDConnectProvider () {
+createIamOpenIdConnectProvider () {
+  echo "Creating OpenIDConnectProvider on AWS [IAM]..."
   eksctl utils associate-iam-oidc-provider \
-    --cluster="$K8S_CLUSTER_NAME" \
+    --cluster="$KUBERNETES_CLUSTER_NAME" \
     --approve
 }
 
-createAWSLBControllerPolicy () {
-  local POLICY_FILEPATH="$TMP_DIR/iam_policy.json"
-  curl -o "$POLICY_FILEPATH" https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.1/docs/install/iam_policy.json
+createAwsLbControllerPolicy () {
+  local POLICY_FILEPATH="$THIS_DIR/iam_policy.json"
+  local POLICY_URL="https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.1/docs/install/iam_policy.json"
+
+  curl -o "$POLICY_FILEPATH" "$POLICY_URL"
+
+  echo "Creating policy for AWSLBController on AWS [IAM]..."
   aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
-    --policy-document "file://$POLICY_FILEPATH" && \
+    --policy-document "file://$POLICY_FILEPATH"
+
   rm "$POLICY_FIPATH"
 }
 
-createAWSLBServiceAccount () {
+createAwsLbServiceAccount () {
   local AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
+  echo "Creating service account for AWSLBController on AWS [EKS]..."
   eksctl create iamserviceaccount \
-    --cluster="$K8S_CLUSTER_NAME" \
+    --cluster="$KUBERNETES_CLUSTER_NAME" \
     --namespace=kube-system \
     --name=aws-load-balancer-controller \
     --role-name "AmazonEKSLoadBalancerControllerRole" \
@@ -50,12 +57,13 @@ createAWSLBServiceAccount () {
     --approve
 }
 
-createAWSLBController () {
+createAwsLbController () {
+  echo "Creating AWSLBController on AWS [EKS]..."
   helm repo add eks https://aws.github.io/eks-charts && \
   helm repo update && \
   helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
     -n kube-system \
-    --set clusterName="$K8S_CLUSTER_NAME" \
+    --set clusterName="$KUBERNETES_CLUSTER_NAME" \
     --set serviceAccount.create=false \
     --set serviceAccount.name=aws-load-balancer-controller
 
@@ -63,19 +71,18 @@ createAWSLBController () {
     -n kube-system aws-load-balancer-controller
 }
 
-createK8Scluster () {
+createKubernetesCluster () {
   local AWS_REGION=$(aws configure get region)
 
-  echo "Creating \"$K8S_CLUSTER_NAME\" k8s cluster on AWS [EKS]..."
-
-  echo "Public CIDR range is required! (to access Kubernetes API endpoint after cluster creation)"
-  read -p "Input your public CIDR: " "K8S_API_ENDPOINT_PUBLIC_ACCESS_CIDR"
+  echo "Creating \"$KUBERNETES_CLUSTER_NAME\" k8s cluster on AWS [EKS]..."
+  echo "Public CIDR range is required! (to access k8s api endpoint after cluster creation)"
+  read -p "Input your public CIDR: " "KUBERNETES_API_ENDPOINT_PUBLIC_ACCESS_CIDR"
 
   cat <<EOF | eksctl create cluster -f -
   apiVersion: eksctl.io/v1alpha5
   kind: ClusterConfig
   metadata:
-    name: ${K8S_CLUSTER_NAME}
+    name: ${KUBERNETES_CLUSTER_NAME}
     region: ${AWS_REGION}
   managedNodeGroups:
     - name: Standard-Workers
@@ -96,32 +103,32 @@ createK8Scluster () {
       privateAccess: true
     publicAccessCIDRs:
       # VPN
-      - ${K8S_API_ENDPOINT_PUBLIC_ACCESS_CIDR}
+      - ${KUBERNETES_API_ENDPOINT_PUBLIC_ACCESS_CIDR}
 EOF
 }
 
-deleteK8Scluster () {
+deleteKubernetesCluster () {
   # DB and DBsubnetGroup must be deleted prior to cluster deletion otherwise eksctl will fail to cleanly delete the cluster
-  deleteDBinstance
-  deleteDBsubnetGroup "$K8S_CLUSTER_NAME"
+  deleteDbInstance
+  createDbSubnetGroup "$KUBERNETES_CLUSTER_NAME"
 
-  echo "Deleting \"$K8S_CLUSTER_NAME\" k8s cluster from AWS..."
-  eksctl delete cluster "$K8S_CLUSTER_NAME" --wait
+  echo "Deleting \"$KUBERNETES_CLUSTER_NAME\" k8s cluster from AWS [EKS]..."
+  eksctl delete cluster "$KUBERNETES_CLUSTER_NAME" --wait
 }
 
-setupK8Scluster () {
+setupKubernetesCluster () {
   isInstalled eksctl && isInstalled helm && isAwsConfigured
 
-  local CREATE_AWS_LB_CONTROLLER="createIAMOpenIDConnectProvider && createAWSLBControllerPolicy && createAWSLBServiceAccount && createAWSLBController"
+  local CREATE_AWS_LB_CONTROLLER="createIamOpenIdConnectProvider && createAwsLbControllerPolicy && createAwsLbServiceAccount && createAwsLbController"
 
-  if eksctl get cluster --name "$K8S_CLUSTER_NAME"; then
+  if eksctl get cluster --name "$KUBERNETES_CLUSTER_NAME"; then
     confirmAction \
-    "==> Delete existing \"$K8S_CLUSTER_NAME\" k8s cluster and create a new one" \
-    "deleteK8Scluster && createK8Scluster && $CREATE_AWS_LB_CONTROLLER"
+    "==> Delete existing \"$KUBERNETES_CLUSTER_NAME\" k8s cluster and create a new one" \
+    "deleteKubernetesCluster && createKubernetesCluster && $CREATE_AWS_LB_CONTROLLER"
   else
     confirmAction \
-    "==> Create new \"$K8S_CLUSTER_NAME\" k8s cluster" \
-    "createK8Scluster && $CREATE_AWS_LB_CONTROLLER"
+    "==> Create new \"$KUBERNETES_CLUSTER_NAME\" k8s cluster" \
+    "createKubernetesCluster && $CREATE_AWS_LB_CONTROLLER"
   fi
 }
 
@@ -133,15 +140,14 @@ getHostedZoneId () {
 }
 
 createHostedZone () {
-  echo "Creating \"$DOMAIN_NAME\" hosted zone on AWS [Route53]..."
-
+  echo "Creating \"$DOMAIN_NAME\" hosted zone on AWS [R53]..."
   aws route53 create-hosted-zone \
     --name "$DOMAIN_NAME" \
     --caller-reference "$IDEMPOTENCY_TOKEN"
 }
 
 deleteHostedZone () {
-  echo "Deleting \"$DOMAIN_NAME\" hosted zone from AWS..."
+  echo "Deleting \"$DOMAIN_NAME\" hosted zone from AWS [R53]..."
   aws route53 delete-hosted-zone "$DOMAIN_NAME" --wait
 }
 
@@ -149,7 +155,7 @@ setupHostedZone () {
   createHostedZone
 }
 
-getManagedCertARN () {
+getManagedCertArn () {
   local SERVICE_DOMAIN_NAME="$1"
 
   aws acm list-certificates \
@@ -200,7 +206,6 @@ createManagedCert () {
   local SERVICE_DOMAIN_NAME="$1"
 
   echo "Creating certificate for $SERVICE_DOMAIN_NAME on AWS [ACM]..."
-
   local CERT_ARN=$(aws acm request-certificate \
     --domain-name "$SERVICE_DOMAIN_NAME" \
     --validation-method DNS \
@@ -238,10 +243,10 @@ deleteManagedCert () {
     --certificate-arn "$CERT_ARN"
 }
 
-writeCertARN () {
+writeCertArn () {
   local SERVICE_NAME="$1"
   local SERVICE_DOMAIN_NAME="$2"
-  local CERT_ARN=$(getManagedCertARN "$SERVICE_DOMAIN_NAME")
+  local CERT_ARN=$(getManagedCertArn "$SERVICE_DOMAIN_NAME")
   local SERVICE_CERT_ARN_PATH="$SECRETS_DIR/$SERVICE/cert-arn.txt"
 
   echo "Saving $CERT_ARN to $SERVICE_CERT_ARN_PATH"
@@ -251,67 +256,67 @@ writeCertARN () {
 setupManagedCerts () {
   for SERVICE in rtl lsp; do
     local SERVICE_DOMAIN_NAME=$(cat "$SECRETS_DIR/$SERVICE/domain-name.txt")
-    local CERT_ARN=$(getManagedCertARN "$SERVICE_DOMAIN_NAME")
+    local CERT_ARN=$(getManagedCertArn "$SERVICE_DOMAIN_NAME")
      
     if [ -n "$CERT_ARN" ]; then
       confirmAction \
         "==> Delete existing \"$SERVICE_DOMAIN_NAME\" cert and create a new one" \
-        "deleteManagedCert $CERT_ARN && createManagedCert $SERVICE_DOMAIN_NAME && writeCertARN $SERVICE $SERVICE_DOMAIN_NAME"
+        "deleteManagedCert $CERT_ARN && createManagedCert $SERVICE_DOMAIN_NAME && writeCertArn $SERVICE $SERVICE_DOMAIN_NAME"
     else
       confirmAction \
         "==> Create new \"$SERVICE_DOMAIN_NAME\" cert" \
-        "createManagedCert $SERVICE_DOMAIN_NAME && writeCertARN $SERVICE $SERVICE_DOMAIN_NAME"
+        "createManagedCert $SERVICE_DOMAIN_NAME && writeCertArn $SERVICE $SERVICE_DOMAIN_NAME"
     fi
   done
 }
 
 getVpcId () {
   aws eks describe-cluster \
-    --name "$K8S_CLUSTER_NAME" \
+    --name "$KUBERNETES_CLUSTER_NAME" \
     --query "cluster.resourcesVpcConfig.vpcId" \
     --output text
 }
 
 getSecurityGroupId () {
-  local VPC_ID=$(getVpcId "$K8S_CLUSTER_NAME")
+  local VPC_ID=$(getVpcId "$KUBERNETES_CLUSTER_NAME")
 
   aws ec2 describe-security-groups \
-    --filters "Name=group-name,Values=eks-cluster-sg-$K8S_CLUSTER_NAME*" "Name=vpc-id,Values=$VPC_ID" \
+    --filters "Name=group-name,Values=eks-cluster-sg-$KUBERNETES_CLUSTER_NAME*" "Name=vpc-id,Values=$VPC_ID" \
     --query "SecurityGroups[0].GroupId" --output text
 }
 
-createDBsubnetGroup () {
+createDbSubnetGroup () {
   local SUBNET_GROUP_NAME="$1"
-  local VPC_ID=$(getVpcId "$K8S_CLUSTER_NAME")
+  local VPC_ID=$(getVpcId "$KUBERNETES_CLUSTER_NAME")
 
   local PRIVATE_SUBNETS_ID=$(aws ec2 describe-subnets \
-    --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=eksctl-$K8S_CLUSTER_NAME-cluster/SubnetPrivate*" \
+    --filters "Name=vpc-id,Values=$VPC_ID" "Name=tag:Name,Values=eksctl-$KUBERNETES_CLUSTER_NAME-cluster/SubnetPrivate*" \
     --query 'Subnets[*].SubnetId' \
     --output json | jq -c .)
 
-  echo "Creating \"$SUBNET_GROUP_NAME\" database subnet group on AWS..."
+  echo "Creating \"$SUBNET_GROUP_NAME\" database subnet group on AWS [RDS]..."
   aws rds create-db-subnet-group \
     --db-subnet-group-name "$SUBNET_GROUP_NAME" \
     --db-subnet-group-description "$SUBNET_GROUP_NAME" \
     --subnet-ids "$PRIVATE_SUBNETS_ID"
 }
 
-deleteDBsubnetGroup () {
+createDbSubnetGroup () {
   local SUBNET_GROUP_NAME="$1"
 
-  echo "Deleting \"$SUBNET_GROUP_NAME\" database subnet group from AWS..."
+  echo "Deleting \"$SUBNET_GROUP_NAME\" database subnet group from AWS [RDS]..."
   aws rds delete-db-subnet-group \
     --db-subnet-group-name "$SUBNET_GROUP_NAME"
 }
 
-createDBinstance () {
+createDbInstance () {
   local DB_PASSWORD=$(cat "$POSTGRES_PATH/dbpassword.txt")
-  local SG_ID=$(getSecurityGroupId "$K8S_CLUSTER_NAME")
+  local SG_ID=$(getSecurityGroupId "$KUBERNETES_CLUSTER_NAME")
 
-  echo "Creating \"$DB_INSTANCE_NAME\" database instance on AWS..."
+  echo "Creating \"$DATABASE_INSTANCE_NAME\" database instance on AWS [RDS]..."
   aws rds create-db-instance \
     --engine postgres \
-    --db-instance-identifier "$DB_INSTANCE_NAME" \
+    --db-instance-identifier "$DATABASE_INSTANCE_NAME" \
     --allocated-storage 20 \
     --db-instance-class "db.t3.micro" \
     --no-publicly-accessible \
@@ -319,70 +324,70 @@ createDBinstance () {
     --no-multi-az \
     --no-storage-encrypted \
     --no-deletion-protection \
-    --master-username "$DB_USERNAME" \
+    --master-username "$DATABASE_USERNAME" \
     --master-user-password "$DB_PASSWORD" \
-    --db-subnet-group-name "$K8S_CLUSTER_NAME" \
+    --db-subnet-group-name "$KUBERNETES_CLUSTER_NAME" \
     --vpc-security-group-ids "$SG_ID"
 
-  echo "Waiting until \"$DB_INSTANCE_NAME\" database instance is fully ready..."
+  echo "Waiting until \"$DATABASE_INSTANCE_NAME\" database instance is fully ready..."
   aws rds wait db-instance-available \
-    --db-instance-identifier="$DB_INSTANCE_NAME"
+    --db-instance-identifier="$DATABASE_INSTANCE_NAME"
 }
 
-deleteDBinstance () {
-  echo "Deleting \"$DB_INSTANCE_NAME\" database instance from AWS..."
+deleteDbInstance () {
+  echo "Deleting \"$DATABASE_INSTANCE_NAME\" database instance from AWS [RDS]..."
   aws rds delete-db-instance \
-    --db-instance-identifier "$DB_INSTANCE_NAME" \
+    --db-instance-identifier "$DATABASE_INSTANCE_NAME" \
     --skip-final-snapshot \
     --delete-automated-backups
 
-  echo "Waiting until \"$DB_INSTANCE_NAME\" database instance is fully deleted..."
+  echo "Waiting until \"$DATABASE_INSTANCE_NAME\" database instance is fully deleted..."
   aws rds wait db-instance-deleted \
-    --db-instance-identifier="$DB_INSTANCE_NAME"
+    --db-instance-identifier="$DATABASE_INSTANCE_NAME"
 }
 
-writeDBURI () {
+writeDbUri () {
   local DB_PASSWORD=$(cat "$POSTGRES_PATH/dbpassword.txt")
   local DB_HOST=$(aws rds describe-db-instances \
-    --filters "Name=db-instance-id,Values=$DB_INSTANCE_NAME" \
+    --filters "Name=db-instance-id,Values=$DATABASE_INSTANCE_NAME" \
     --query "*[].[Endpoint.Address,Endpoint.Port]" \
     --output json | jq -c -r .[0][0])
-  local DB_URI="postgresql://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/postgres"
+  local DB_URI="postgresql://$DATABASE_USERNAME:$DB_PASSWORD@$DB_HOST/postgres"
   local DB_CONN_PATH="$POSTGRES_PATH/conn.txt"
 
   echo -n "$DB_URI" > "$DB_CONN_PATH"
   echo "Saved connection details to $DB_CONN_PATH"
 }
 
-setupDBInstance () {
+setupDbInstance () {
   isInstalled aws && isAwsConfigured
 
   local DB_INSTANCE_EXISTS=$(aws rds describe-db-instances \
-    --filters "Name=db-instance-id,Values=$DB_INSTANCE_NAME" \
+    --filters "Name=db-instance-id,Values=$DATABASE_INSTANCE_NAME" \
     --query 'DBInstances[*].[DBInstanceIdentifier]' \
     --output text)
 
-  local CREATE_DB="createDBsubnetGroup $K8S_CLUSTER_NAME && createDBinstance && writeDBURI"
-  local DELETE_DB="deleteDBinstance && deleteDBsubnetGroup $K8S_CLUSTER_NAME"
+  local CREATE_DB="createDbSubnetGroup $KUBERNETES_CLUSTER_NAME && createDbInstance && writeDbUri"
+  local DELETE_DB="deleteDbInstance && createDbSubnetGroup $KUBERNETES_CLUSTER_NAME"
 
   if [ -n "$DB_INSTANCE_EXISTS" ]; then
     confirmAction \
-    "==> Delete existing \"$DB_INSTANCE_NAME\" database instance and create a new one" \
+    "==> Delete existing \"$DATABASE_INSTANCE_NAME\" database instance and create a new one" \
     "$DELETE_DB && $CREATE_DB"
   else
     confirmAction \
-    "==> Create new \"$DB_INSTANCE_NAME\" database instance" \
+    "==> Create new \"$DATABASE_INSTANCE_NAME\" database instance" \
     "$CREATE_DB"
   fi
 }
 
-getK8SServiceExternalIP () {
+getKubernetesServiceExternalIP () {
   local SERVICE_NAME="$1"
 
   kubectl get svc "$SERVICE_NAME" -o json | jq -r ".status.loadBalancer.ingress[].hostname"
 }
 
-getK8SIngressExternalIP () {
+getKubernetesIngressExternalIP () {
   local INGRESS_NAME="$1"
 
   kubectl get ingress "$INGRESS_NAME" -o json | jq -r ".status.loadBalancer.ingress[].hostname"
@@ -391,7 +396,7 @@ getK8SIngressExternalIP () {
 setupDNSRecords () {
   for SERVICE in bitcoind lnd lsp; do
     local SERVICE_DOMAIN_NAME=$(cat "$SECRETS_DIR/$SERVICE/domain-name.txt")
-    local SERVICE_EXTERNAL_IP=$(getK8SServiceExternalIP $SERVICE)
+    local SERVICE_EXTERNAL_IP=$(getKubernetesServiceExternalIP "$SERVICE")
 
     echo "Adding CNAME DNS record for $SERVICE_DOMAIN_NAME k8s service..."
 
@@ -399,7 +404,7 @@ setupDNSRecords () {
   done
 
   local RTL_DOMAIN_NAME=$(cat "$RTL_PATH/domain-name.txt")
-  local RTL_EXTERNAL_IP=$(getK8SIngressExternalIP rtl)
+  local RTL_EXTERNAL_IP=$(getKubernetesIngressExternalIP rtl)
 
   echo "Adding CNAME DNS record for $RTL_DOMAIN_NAME k8s ingress.."
 
@@ -432,9 +437,9 @@ getElbListenerArn () {
 deleteElbListener () {
   local LISTENER_ARN="$1"
 
-  echo "Deleting \"$LISTENER_ARN\" ELB listener from AWS..."
-
-  aws elbv2 delete-listener --listener-arn "$LISTENER_ARN"
+  echo "Deleting \"$LISTENER_ARN\" ELB listener from AWS [ELB]..."
+  aws elbv2 delete-listener \
+    --listener-arn "$LISTENER_ARN"
 }
 
 setupElbListener () {
@@ -463,10 +468,10 @@ confirmAction \
 
 writeDomainName
 checkRequiredFiles # TODO: dont use func here, check for file existence separately!! (or some files need to be always present?!)
-setupK8Scluster
+setupKubernetesCluster
 setupHostedZone
 setupManagedCert
-setupDBInstance
+setupDbInstance
 
 echo "==> Checking that db connection details are saved"
 checkFileExistsNotEmpty "$POSTGRES_PATH/conn.txt" 
@@ -507,6 +512,6 @@ echo "==> Waiting until containers are ready"
 sh "$THIS_DIR/k8s-wait.sh" "rtl lsp"
 
 setupDNSRecords
-setupELBListeners
+setupElbListeners
 
 echo "==> Setup for $BITCOIN_NETWORK on $CLOUD_PROVIDER has been completed!"
