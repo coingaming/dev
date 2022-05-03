9216cb61b59f73235912a5e0c3a4a5db7e26840e
drownbes <drownbes@gmail.com>
2022-05-03 17:39:39 +0300

    build everything

--- nix/ci-release.sh
+++ nix/ci-release.sh
@@ -12,7 +12,7 @@ ls -la
 echo "btc-lsp ==> Binaries build"
 nix-build btc-lsp/nix/docker-image-electrs.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-electrs.tar.gz"
 nix-build btc-lsp/nix/docker.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-btc-lsp.tar.gz"
-#nix-build btc-lsp/nix/docker-integration.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz"
+nix-build btc-lsp/nix/docker-integration.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz"
 
 echo "btc-lsp ==> Docker btc-lsp image verification"
 docker load -q -i \
@@ -28,9 +28,9 @@ docker load -q -i \
   | tr -d '\n' \
   > "$BTC_LSP_BUILD_DIR/docker-image-electrs.txt"
 
-#echo "integration ==> Docker integration image verification"
-#docker load -q -i \
-#  "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz" \
-#  | awk '{print $NF}' \
-#  | tr -d '\n' \
-#  > "$BTC_LSP_BUILD_DIR/docker-image-integration.txt"
+echo "integration ==> Docker integration image verification"
+docker load -q -i \
+  "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz" \
+  | awk '{print $NF}' \
+  | tr -d '\n' \
+  > "$BTC_LSP_BUILD_DIR/docker-image-integration.txt"
