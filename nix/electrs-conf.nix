1bd0cf5c61bab77d93194a06dadc2d4a7943d0c3
Artem <drownbes@gmail.com>
2022-05-09 23:08:38 +0300

    WIP: add postgres

---
+++ nix/electrs-conf.nix
@@ -0,0 +1,49 @@
+{
+  dataDir ? "./tmp"
+, bitcoinDir
+, name
+, writeText
+, writeShellScriptBin
+, symlinkJoin
+, runCommand
+, openssl
+, electrs
+}:
+let
+  electrstoml = writeText "electrs.toml" ''
+    auth="developer:developer"
+    log_filters = "INFO"
+  '';
+  serviceName = "electrs-${name}";
+  workDir = "${dataDir}/${serviceName}";
+  setup = writeShellScriptBin "setup" ''
+    mkdir -p "${workDir}/db"
+    cp -f ${electrstoml} ${workDir}/electrst.toml
+  '';
+  start = writeShellScriptBin "start" ''
+    ${electrs}/bin/electrs \
+    --conf="${workDir}/electrs.toml" \
+    --db-dir="${workDir}/db" \
+    --daemon-dir="${workDir}/${bitcoinDir}" \
+    --network=regtest \
+    --electrum-rpc-addr="127.0.0.1:60401" \
+    --daemon-rpc-addr="127.0.0.1:18443" \
+    --timestamp \
+    > ${workDir}/stdout.log 2>&1 &
+    echo "$!" > "${workDir}/electrs.pid"
+  '';
+  stop = writeShellScriptBin "stop" ''
+    electrs_pid=`cat ${workDir}/electrs.pid`
+    echo "Stoping electrs ${name} $electrs_pid"
+    kill -9 "$electrs_pid"
+  '';
+  up = writeShellScriptBin "up" ''
+    ${setup}/bin/setup
+    ${start}/bin/start
+  '';
+  down = writeShellScriptBin "down" ''
+    ${stop}/bin/stop
+  '';
+in {
+  inherit up down;
+}
--- test.nix
+++ test.nix
@@ -4,6 +4,8 @@ let
   prjSrc = (import ./nix/project.nix).prjSrc;
   bc = import ./nix/bitcoin-conf.nix;
   ln = import ./nix/lnd-conf.nix;
+  electrs = import ./nix/electrs-conf.nix;
+  pg = import ./nix/postgres-conf.nix;
   bitcoindConf = nixPkgs.callPackage bc {
     name="alice";
     dataDir=".";
@@ -32,6 +34,15 @@ let
     name = "bob";
     macaroonDir = "${prjSrc}/btc-lsp/test/Macaroon/";
   };
+  electrsAlice = nixPkgs.callPackage electrs {
+    dataDir = ".";
+    name = "alice";
+    bitcoinDir = "bitcoind_alice";
+  };
+  postgres = nixPkgs.callPackage pg {
+    dataDir = ".";
+    name = "postgres_data";
+  };
   networkBitcoinTest = p.network-bitcoin.components.tests.network-bitcoin-tests;
   genericPrettyInstancesTest = p.generic-pretty-instances.components.tests.generic-pretty-instances-test;
   btcLspTest = p.btc-lsp.components.tests.btc-lsp-test;
@@ -49,18 +60,25 @@ in {
     ${genericPrettyInstancesTest}/bin/generic-pretty-instances-test > $out
     echo $?
   '';
-  btc-lsp-test =  nixPkgs.runCommand "btc-lsp-test" {} ''
+  btc-lsp-test =  nixPkgs.runCommand "btc-lsp-test" {
+    buildInputs=[nixPkgs.ps];
+  } ''
     set -euo pipefail
     ${bitcoindConf.up}/bin/up
     ${lndLsp.up}/bin/up
     ${lndAlice.up}/bin/up
     ${lndBob.up}/bin/up
+    ${electrsAlice.up}/bin/up
+    ${postgres.up}/bin/up
 
+    ps aux | grep electrs
     ${btcLspTest}/bin/btc-lsp-test && true
     pwd
     ls -la
     echo "end"
 
+    ${postgres.down}/bin/down
+    ${electrsAlice.down}/bin/down
     ${lndLsp.down}/bin/down
     ${lndAlice.down}/bin/down
     ${lndBob.down}/bin/down
