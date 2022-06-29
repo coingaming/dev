1edeea23cb052d67b77405a2ec77bb324346a9ce
tkachuk-labs <tkachuk.labs@gmail.com>
2022-06-29 17:08:48 +0300

    safe mainnet seed generation procedure

--- nix/seed-shell.sh
+++ nix/seed-shell.sh
@@ -2,17 +2,4 @@
 
 set -e
 
-(
-  mkdir -p ~/Downloads
-  cd ~/Downloads
-  sudo apt-get install -y gnupg2 curl tar
-  curl -o install-nix-2.3.10 \
-    https://releases.nixos.org/nix/nix-2.3.10/install
-  curl -o install-nix-2.3.10.asc \
-    https://releases.nixos.org/nix/nix-2.3.10/install.asc
-  gpg2 \
-    --keyserver hkps://keyserver.ubuntu.com \
-    --recv-keys B541D55301270E0BCF15CA5D8170B4726D7198DE
-  gpg2 --verify ./install-nix-2.3.10.asc
-  sh ./install-nix-2.3.10
-)
+nix-shell --pure -p '(import (fetchTarball { url = "https://github.com/coingaming/src/tarball/ef7ed491e5102063763538bc4cb2e86bc8021973"; sha256 = "1ml6jrz605jx7pmzd38d1490n8cbjv8yaclpqy57j7lqp1raaa7w"; } + "/nix/project.nix")).nixBitcoin.lndinit'
