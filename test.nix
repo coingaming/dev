b450ffb0b4f40455569584097530f6768ecb4121
Mikhail Prushinskiy <mikhail.prushinskiy@heathmont.net>
2022-07-04 15:05:02 +0300

    Implement separate startAll/stopAll for electrs

--- test.nix
+++ test.nix
@@ -24,7 +24,6 @@ in {
     ${deps.startAll}/bin/start-test-deps
     source ${deps.envFile}
     ${btcLspTest}/bin/btc-lsp-test 2>&1 | tee $out
-    ${deps.stopAll}/bin/stop-test-deps
   '';
   electrs-client-test =  nixPkgs.runCommand "electrs-client-test" ({
     buildInputs=[nixPkgs.ps];
