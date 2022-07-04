e5f94b88dbfc7f8436d1d128a204460aa5ace959
Mikhail Prushinskiy <mikhail.prushinskiy@heathmont.net>
2022-07-04 10:39:32 +0300

    Try to avoid starting deps second time

--- test.nix
+++ test.nix
@@ -24,12 +24,10 @@ in {
     ${deps.startAll}/bin/start-test-deps
     source ${deps.envFile}
     ${btcLspTest}/bin/btc-lsp-test 2>&1 | tee $out
-    ${deps.stopAll}/bin/stop-test-deps
   '';
   electrs-client-test =  nixPkgs.runCommand "electrs-client-test" ({
     buildInputs=[nixPkgs.ps];
   }) ''
-    ${deps.startAll}/bin/start-test-deps
     source ${deps.envFile}
     ${electrsClientTest}/bin/electrs-client-test 2>&1 | tee $out
     ${deps.stopAll}/bin/stop-test-deps
