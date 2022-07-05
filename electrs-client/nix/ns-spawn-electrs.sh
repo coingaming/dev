4dc61b071a72e1200b20b39b7d87e0300ff5ecac
Mikhail Prushinskiy <mikhail.prushinskiy@heathmont.net>
2022-07-05 14:35:51 +0300

    Fix electrs docker path

---
+++ electrs-client/nix/ns-spawn-electrs.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+THIS_DIR="$(dirname "$(realpath "$0")")"
+
+ELECTRS_DIR="$THIS_DIR/../build/shell/electrs"
+BITCOIN_DIR="$THIS_DIR/../build/shell/bitcoind"
+
+echo "==> starting electrs..."
+
+mkdir -p "$ELECTRS_DIR/db"
+
+electrs \
+  --conf="$ELECTRS_DIR/electrs.toml" \
+  --db-dir="$ELECTRS_DIR/db" \
+  --daemon-dir="$BITCOIN_DIR" \
+  --network=regtest \
+  --electrum-rpc-addr="127.0.0.1:60401" \
+  --daemon-rpc-addr="127.0.0.1:18443" \
+  --timestamp \
+  > $ELECTRS_DIR/stdout.log 2>&1 & \
+  echo "$!" > "$ELECTRS_DIR/electrs.pid"
+
+  #--jsonrpc-import
+  #--wait-duration-secs
+  #--index-batch-size
+  #--bulk-index-threads
+  #--tx-cache-size-mb
+  #--blocktxids-cache-size-mb
+  #--txid-limit
+  #--server-banner
+  #--conf
+  #--cookie-file
+  #--monitoring-addr
+
+echo "==> electrs has been started!"
