60201d6ead71a8f80bbda45339d5cb485b19d65e
Mikhail Prushinskiy <mikhail.prushinskiy@heathmont.net>
2022-07-05 10:16:56 +0300

    Clean deps list

--- electrs-client/src/ElectrsClient/Import/External.hs
+++ electrs-client/src/ElectrsClient/Import/External.hs
@@ -3,36 +3,12 @@ module ElectrsClient.Import.External
   )
 where
 
-import Chronos as X (Timespan (..), stopwatch)
-import Control.Concurrent.Async as X
-  ( Async,
-    asyncThreadId,
-    cancel,
-    linkOnly,
-    wait,
-    waitAnyCancel,
-    waitAnySTM,
-  )
-import Control.Concurrent.STM as X (atomically)
-import Control.Concurrent.STM.TChan as X
-  ( TChan,
-    dupTChan,
-    newBroadcastTChan,
-    newBroadcastTChanIO,
-    readTChan,
-    writeTChan,
-  )
-import Control.Concurrent.Thread.Delay as X (delay)
-import Control.Error.Util as X (failWith, failWithM)
-import Control.Monad.Extra as X (eitherM, maybeM)
 import Control.Monad.Trans.Except as X
   ( catchE,
     except,
     throwE,
     withExceptT,
   )
-import Crypto.Hash as X (Digest, SHA256 (..), hashWith, hashlazy)
-import Crypto.Random as X (getRandomBytes)
 import Data.Aeson as X
   ( FromJSON (..),
     FromJSONKey (..),
@@ -44,84 +20,11 @@ import Data.Aeson as X
     genericParseJSON,
   )
 import Data.Coerce as X (coerce)
-import Data.Containers.ListUtils as X (nubOrd)
-import Data.Either.Extra as X (fromEither)
-import Data.EitherR as X (flipET, handleE)
-import Data.List as X (partition)
-import Data.List.Extra as X (notNull)
-import Data.Maybe as X (fromJust)
-import Data.Pool as X (Pool, destroyAllResources)
---import Data.ProtoLens as X (defMessage)
---import Data.ProtoLens.Encoding as X (decodeMessage, encodeMessage)
-import Data.Ratio as X ((%))
 import Data.Text as X (pack, unpack)
-import Data.Time.Clock as X
-  ( DiffTime,
-    UTCTime,
-    addUTCTime,
-    diffTimeToPicoseconds,
-    diffUTCTime,
-    secondsToDiffTime,
-  )
-import Data.Tuple.Extra as X (uncurry3)
-import Data.Type.Equality as X (type (==))
-import Data.UUID as X (UUID)
-import Database.Esqueleto.Legacy as X
-  ( Entity (..),
-  )
-import GHC.Exts as X (IsList)
-import Katip as X
-  ( ColorStrategy (..),
-    Environment (..),
-    Katip (..),
-    KatipContext (..),
-    KatipContextT,
-    LogContexts,
-    LogEnv,
-    LogStr (..),
-    Namespace,
-    Severity (..),
-    Verbosity (..),
-    bracketFormat,
-    closeScribes,
-    defaultScribeSettings,
-    initLogEnv,
-    jsonFormat,
-    katipAddContext,
-    logStr,
-    logTM,
-    mkHandleScribeWithFormatter,
-    permitItem,
-    registerScribe,
-    runKatipContextT,
-    sl,
-  )
-import Katip.Format.Time as X
-  ( formatAsLogTime,
-  )
 import LndClient as X
   ( MSat (..),
   )
---import LndClient.Util as X
---  ( spawnLink,
---    txIdHex,
---    withSpawnLink,
---  )
---import Network.GRPC.Client as X (CompressMode (..))
---import Network.HTTP2.Client2 as X (HostName, PortNumber)
-import Text.Casing as X (camel)
 import Text.PrettyPrint.GenericPretty as X (Out (..))
---import Text.PrettyPrint.GenericPretty.Import as X
---  ( PrettyLog (..),
---    SecretVision (..),
---    inspect,
---    inspectGenPlain,
---    inspectPlain,
---    inspectStr,
---    inspectStrPlain,
---  )
---import Text.PrettyPrint.GenericPretty.Instance as X ()
-import Text.Shakespeare.I18N as X (ToMessage (..))
 import Universum as X hiding
   ( atomically,
     bracket,
