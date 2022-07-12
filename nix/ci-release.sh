71bbe0c6d7183957705571fbf1b71fe71a635060
tkachuk-labs <tkachuk.labs@gmail.com>
2022-07-12 12:40:38 +0300

    fix ci

--- nix/ci-release.sh
+++ nix/ci-release.sh
@@ -12,9 +12,9 @@ pwd
 ls -la
 
 echo "btc-lsp ==> Binaries build"
-nix-build electrs-client/nix/docker-image-electrs.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-electrs.tar.gz"
 nix-build btc-lsp/nix/docker.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-btc-lsp.tar.gz"
 nix-build btc-lsp/nix/docker-integration.nix --out-link "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz"
+nix-build electrs-client/nix/docker-image-electrs.nix --out-link "$ELECTRS_CLIENT_BUILD_DIR/docker-image-electrs.tar.gz"
 
 echo "btc-lsp ==> Docker btc-lsp image verification"
 docker load -q -i \
@@ -23,16 +23,16 @@ docker load -q -i \
   | tr -d '\n' \
   > "$BTC_LSP_BUILD_DIR/docker-image-btc-lsp.txt"
 
-echo "electrs ==> Docker electrs image verification"
-docker load -q -i \
-  "$ELECTRS_CLIENT_BUILD_DIR/docker-image-electrs.tar.gz" \
-  | awk '{print $NF}' \
-  | tr -d '\n' \
-  > "$ELECTRS_CLIENT_BUILD_DIR/docker-image-electrs.txt"
-
 echo "integration ==> Docker integration image verification"
 docker load -q -i \
   "$BTC_LSP_BUILD_DIR/docker-image-integration.tar.gz" \
   | awk '{print $NF}' \
   | tr -d '\n' \
   > "$BTC_LSP_BUILD_DIR/docker-image-integration.txt"
+
+echo "electrs ==> Docker electrs image verification"
+docker load -q -i \
+  "$ELECTRS_CLIENT_BUILD_DIR/docker-image-electrs.tar.gz" \
+  | awk '{print $NF}' \
+  | tr -d '\n' \
+  > "$ELECTRS_CLIENT_BUILD_DIR/docker-image-electrs.txt"
