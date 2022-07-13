4a0696301c126be505d10a544fb99e76f300cc8f
tkachuk-labs <tkachuk.labs@gmail.com>
2022-07-13 15:15:44 +0300

    nix-shell helper

---
+++ nix/hm-shell-native.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+THIS_DIR="$(dirname "$(realpath "$0")")"
+
+nix-shell \
+  "$THIS_DIR/../shell.nix" \
+  --option extra-substituters "https://cache.nixos.org https://hydra.iohk.io" \
+  --option trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
