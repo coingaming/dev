2c184af6ddf3debdd0b49f488e7a2885025539e2
Artem Markov <drownbes@gmail.com>
2022-12-08 17:39:30 +0200

    fix

--- nix/upload-to-cache.sh
+++ nix/upload-to-cache.sh
@@ -2,4 +2,4 @@ set -eu
 set -f # disable globbing
 export IFS=' '
 echo "Signing and uploading paths" $OUT_PATHS
-exec nix copy --to 'file:///tmp/nix_ci_cache' $OUT_PATHS
+nix copy --to 'file:///tmp/nix_ci_cache' $OUT_PATHS
