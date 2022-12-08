711c4b7d193f256c0f689a26cc279b4ab1ac7d17
Artem Markov <drownbes@gmail.com>
2022-12-08 17:57:27 +0200

    try naive

--- nix/upload-to-cache.sh
+++ nix/upload-to-cache.sh
@@ -4,4 +4,4 @@ export IFS=' '
 echo $PATH
 pwd
 echo "Signing and uploading paths" $OUT_PATHS
-nix copy --to 'file:///tmp/nix_ci_cache' $OUT_PATHS
+/nix/var/nix/profiles/default/bin/nix copy --to 'file:///tmp/nix_ci_cache' $OUT_PATHS
