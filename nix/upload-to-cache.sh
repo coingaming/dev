321cc212c3f9c426b684c1bca643b53764d1b578
Artem Markov <drownbes@gmail.com>
2022-12-14 10:10:37 +0200

    try

--- nix/upload-to-cache.sh
+++ nix/upload-to-cache.sh
@@ -5,4 +5,4 @@ echo $PATH
 pwd
 echo "Signing and uploading paths" $OUT_PATHS
 /nix/var/nix/profiles/default/bin/nix copy --to 'file:///tmp/nix_ci_cache' $OUT_PATHS
-/nix/var/nix/profiles/default/bin/nix copy --to 'ssh://cache_coinix' $OUT_PATHS
+/nix/var/nix/profiles/default/bin/nix copy --to 'ssh://nix-ssh@nixos.dev.testenv.io' $OUT_PATHS
